c8dcc81eaff1860bf0195b7f58b32e5a3492664d
정지원 <4dakto@gmail.com>
2017-03-22 10:57:59 +0900

    Jiwon :: Added Google logout

--- public/fireinit.js
+++ public/fireinit.js
@@ -1,9 +1,11 @@
+/// <reference path="../node_modules/@types/firebase/index.d.ts" />
+
 // Initialize Firebase
-// TODO: Replace with your project's customized code snippet
 var config = {
-  apiKey: "apiKey",
-  authDomain: "projectId.firebaseapp.com",
-  databaseURL: "https://databaseName.firebaseio.com",
-  storageBucket: "bucket.appspot.com",
+  apiKey: "AIzaSyAMDHT019sZpp8kkmaaebaMht_iV3QAp68",
+  authDomain: "fir-auth-ff24b.firebaseapp.com",
+  databaseURL: "https://fir-auth-ff24b.firebaseio.com",
+  storageBucket: "fir-auth-ff24b.appspot.com",
+  messagingSenderId: "64516457407"
 };
 firebase.initializeApp(config);
